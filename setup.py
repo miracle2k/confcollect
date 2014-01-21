c7ad5a555442395ca22ba3f580f14898cfa8721f
Michael Elsdörfer <michael@elsdoerfer.com>
2014-01-21 11:13:08 +0100

    Actually provide a version number.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="Helpers to collect configuration from os environment, files etc.",
     author='Michael Elsdoerfer',
     author_email='michael@elsdoerfer.com',
-    version="0.1",
+    version="0.1.2",
     license='BSD',
     py_modules=['confcollect'],
     zip_safe=True,
