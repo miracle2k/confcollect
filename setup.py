e6244451d785e8ac28cf7a03bff17deac1a996ea
Michael Elsdörfer <michael@elsdoerfer.com>
2015-01-19 01:34:35 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="Helpers to collect configuration from os environment, files etc.",
     author='Michael Elsdoerfer',
     author_email='michael@elsdoerfer.com',
-    version="0.2",
+    version="0.2.1",
     license='BSD',
     py_modules=['confcollect'],
     zip_safe=True,
